973bda778d5a8c446c6d50706bef4e7b99dc80f7
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-11 16:22:16 -0300

    Animation in modal of event click

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ pizzaJson.map( (item, index) => {
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  pizzaItem.querySelector('a').addEventListener('click', event => {
+    event.preventDefault();
+
+    querySelector('.pizzaWindowArea').style.opacity = 0;
+
+    querySelector('.pizzaWindowArea').style.display = 'flex';
+
+    setTimeout( () => {
+      querySelector('.pizzaWindowArea').style.opacity = 1;
+    }, 300);
+  });
   
   querySelector('.pizza-area').append( pizzaItem );
 });
