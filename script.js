42d2683f7aeb4cde116f5a6b320959f000327c3d
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-11 15:32:19 -0300

    adicionando dados ao html

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const querySelectorAll = elemento => document.querySelectorAll(elemento);
 
 pizzaJson.map( (item, index) => {
   let pizzaItem = querySelector('.models .pizza-item').cloneNode(true);
-  // Preencher as informações em pizzaItem
 
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  
   querySelector('.pizza-area').append( pizzaItem );
 });
